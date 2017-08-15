46970784a0fe4f1263c35fd86004785029b9dcbc
KyleBehiels <kylejamesbehiels@gmail.com>
2017-08-15 12:42:08 -0600

    Fixed circleci config

--- testing/test_hash.py
+++ testing/test_hash.py
@@ -1,15 +1,12 @@
-import unittest, hash
+from modules import hash
+import unittest
 
 
-class FullInstallTest(unittest.TestCase):
-
-    def test_dummy(self):
-        self.assertEqual('hello', 'hello')
+class HashTesting(unittest.TestCase):
 
     def test_hash(self):
         self.assertEqual(hash.hexhash("This is a test string for hexhash"), "46e7d02b9a0d5339")
         self.assertEqual(hash.md5hash("test_constants/test_text_file.txt"), "f5c85408e67ef9a90f3e416863ba84de")
-
 
 if __name__ == '__main__':
     unittest.main()
