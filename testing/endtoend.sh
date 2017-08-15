1a247aceadaf847c89a0456814964da0896527aa
KyleBehiels <kylejamesbehiels@gmail.com>
2017-08-15 16:27:50 -0600

    Added a test for taxid file in endtoend.sh. This along with data being cached will prevent a full install after every commit while still allowing end to end testing

--- testing/endtoend.sh
+++ testing/endtoend.sh
@@ -9,5 +9,16 @@ echo "\n============================="
 echo "Running base install script"
 echo "=============================\n"
 cd ..
-sudo -H bash base_installation.sh
+
+# if statement for avoiding the miserable gid_taxid_prot.dmp download
+if [ ! -f /data/gi_taxid_prot.dmp ];
+then
+    sudo -H bash base_installation.sh
+
+else
+    cd testing
+    sudo -H bash base_installation_for_test.sh
+    cd ..
+fi
+
 sudo -H bash test_metannotate.sh
