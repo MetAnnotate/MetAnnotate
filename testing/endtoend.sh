5970b41befa6920ad85cb143197841f352ef8fda
KyleBehiels <kylejamesbehiels@gmail.com>
2017-08-15 16:44:22 -0600

    Quick commit to add test constants with disabled testing.

--- testing/endtoend.sh
+++ testing/endtoend.sh
@@ -13,9 +13,15 @@ cd ..
 # if statement for avoiding the miserable gid_taxid_prot.dmp download
 if [ ! -f /data/gi_taxid_prot.dmp ];
 then
+    echo "========================================================="
+    echo "No data cache detected, running full base_installation.sh"
+    echo "========================================================="
     sudo -H bash base_installation.sh
 
 else
+    echo "============================================================="
+    echo "Data cache detected, running abbreviated base_installation.sh"
+    echo "============================================================="
     cd testing
     sudo -H bash base_installation_for_test.sh
     cd ..
