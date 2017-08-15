07f91da3e73fb92cf00f3ae01b6c531d5213b051
KyleBehiels <kylejamesbehiels@gmail.com>
2017-08-15 15:57:37 -0600

    Created an end to end test script that handles installation and runs test_metannotate.sh and verifies that the output files were generated properly

--- testing/endtoend.sh
+++ testing/endtoend.sh
@@ -4,7 +4,10 @@
 sudo -S apt-mark hold sudo
 sudo -S apt-mark hold procps
 sudo -S apt-mark hold strace
+
+echo "\n============================="
 echo "Running base install script"
+echo "=============================\n"
 cd ..
 sudo -H bash base_installation.sh
 sudo -H bash test_metannotate.sh
--- testing/test_end_to_end.py
+++ testing/test_end_to_end.py
@@ -1,14 +1,45 @@
 from modules import hash
 import unittest
 import subprocess
-import os
-import sys
+import os, os.path
+import glob
 
 
 class testEndToEnd(unittest.TestCase):
 
+    def getFileContents(mFile):
+        returnable = ''
+
+        for line in mFile:
+            returnable =  returnable + line
+
+        return returnable
+
     def test_endtoend(self):
         subprocess.call("bash endtoend.sh", shell=True)
+        generatedFaFiles = []
+        for filename in glob.glob("../test_output/*.fa"):
+            if filename.__contains__("msa") or filename.__contains__("refseq"):
+                generatedFaFiles.append(open(filename, 'r'))
+
+        referenceFaFiles = [open("test_constants/rpoB_0_msa_0.fa", 'r'),
+                            open("test_constants/rpoB_0_refseq_msa_1.fa", 'r')]
+
+        # To be replaced by generated fa files
+
+
+
+        for i in range(0,2):
+            print 'Comparing reference file: ' + referenceFaFiles[i].name
+            tempStringReference = ''
+            tempStringGenerated = ''
+            for line in referenceFaFiles[i]:
+                tempStringReference = tempStringReference + line
+            for line in generatedFaFiles[i]:
+                tempStringGenerated = tempStringGenerated + line
+            self.assertEqual(tempStringReference, tempStringGenerated)
+
+
 
 if __name__ == '__main__':
     unittest.main()
