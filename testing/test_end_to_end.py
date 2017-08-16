bad35fd6d7ef35cf7fbc78aba3c09060b1267806
KyleBehiels <kylejamesbehiels@gmail.com>
2017-08-16 12:27:57 -0600

    Changed python interpreter

--- testing/test_end_to_end.py
+++ testing/test_end_to_end.py
@@ -18,15 +18,18 @@ class testEndToEnd(unittest.TestCase):
                             open("test_constants/rpoB_0_refseq_msa_1.fa", 'r')]
 
         # compare the two FA files with reference
-        for i in range(0,2):
-            print 'Comparing reference file: ' + referenceFaFiles[i].name
-            tempStringReference = ''
-            tempStringGenerated = ''
-            for line in referenceFaFiles[i]:
-                tempStringReference = tempStringReference.rstrip('\r\n') + line
-            for line in generatedFaFiles[i]:
-                tempStringGenerated = tempStringGenerated.rstrip('\r\n') + line
-            self.assertEqual(tempStringReference, tempStringGenerated)
+        if(len(generatedFaFiles) > 0):
+            for i in range(0,2):
+                print 'Comparing reference file: ' + referenceFaFiles[i].name
+                tempStringReference = ''
+                tempStringGenerated = ''
+                for line in referenceFaFiles[i]:
+                    tempStringReference = tempStringReference.rstrip('\r\n') + line
+                for line in generatedFaFiles[i]:
+                    tempStringGenerated = tempStringGenerated.rstrip('\r\n') + line
+                self.assertEqual(tempStringReference, tempStringGenerated)
+        else:
+            print("Please run test_metannotate.sh and verify that it works before running nosetests")
 
 
 if __name__ == '__main__':
