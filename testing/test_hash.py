3699f49145aee56c79e4ca1cca4e5efb06a8ec31
KyleBehiels <kylejamesbehiels@gmail.com>
2017-08-18 14:35:53 -0700

    Added docstring headers to test_hash.py

--- testing/test_hash.py
+++ testing/test_hash.py
@@ -16,6 +16,13 @@ import unittest
 class HashTesting(unittest.TestCase):
 
     def test_hash(self):
+        """
+        Description: Method that tests the methods 'hexhash()' and 'md5hash()' from module hash
+        
+        :param self: Reference to the HashTesting class
+        :return:
+
+        """
         self.assertEqual(hash.hexhash("This is a test string for hexhash"), "46e7d02b9a0d5339")
         self.assertEqual(hash.md5hash("test_constants/test_text_file.txt"), "f5c85408e67ef9a90f3e416863ba84de")
 
