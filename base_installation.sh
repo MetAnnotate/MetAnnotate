e2266e5c98498868b18f47e4ec4d0faf1ed2a55f
Lee Bergstrand <leemacboy@gmail.com>
2017-08-24 02:26:52 -0400

    Revert previous commenting out and exit one.

--- base_installation.sh
+++ base_installation.sh
@@ -35,26 +35,24 @@ if [ ! `which ktImportText` ] ; then
   cd $metAnnotateDir
 fi
 
+
 # The two possible paths to linuxbrew.
 BREW_PATH_ONE=~/.linuxbrew
 BREW_PATH_TWO=/home/linuxbrew/.linuxbrew
 
+echo -e "\nInstalling Linuxbrew\n"
+if [ ! -d "$BREW_PATH_ONE" ] && [ ! -d "$BREW_PATH_TWO" ] ; then
+    yes | ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
+fi
 
-#echo -e "\nInstalling Linuxbrew\n"
-#if [ ! -d "$BREW_PATH_ONE" ] && [ ! -d "$BREW_PATH_TWO" ] ; then
-#    yes | ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
-#fi
-#
-#echo -e "\nAttempting to add Brew to PATH\n"
-#if [ ! `which brew` ] ; then
-#    echo -e "\nAdding Brew to PATH\n"
-#    test -d ~/.linuxbrew && PATH="$HOME/.linuxbrew/bin:$PATH"
-#    test -d /home/linuxbrew/.linuxbrew && PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
-#    test -r ~/.bash_profile && echo 'export PATH="$(brew --prefix)/bin:$PATH"' >>~/.bash_profile
-#    echo 'export PATH="$(brew --prefix)/bin:$PATH"' >>~/.profile
-#fi
-
-exit 1
+echo -e "\nAttempting to add Brew to PATH\n"
+if [ ! `which brew` ] ; then
+    echo -e "\nAdding Brew to PATH\n"
+    test -d ~/.linuxbrew && PATH="$HOME/.linuxbrew/bin:$PATH"
+    test -d /home/linuxbrew/.linuxbrew && PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
+    test -r ~/.bash_profile && echo 'export PATH="$(brew --prefix)/bin:$PATH"' >>~/.bash_profile
+    echo 'export PATH="$(brew --prefix)/bin:$PATH"' >>~/.profile
+fi
 
 brew tap homebrew/science
 brew install emboss --without-x
