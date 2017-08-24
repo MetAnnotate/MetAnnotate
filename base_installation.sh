bfa9241057672f2ceeccbf6f87faff4455f80ae4
Lee Bergstrand <leemacboy@gmail.com>
2017-08-24 01:15:40 -0400

    Fixed check for previous linux brew install.

--- base_installation.sh
+++ base_installation.sh
@@ -35,13 +35,18 @@ if [ ! `which ktImportText` ] ; then
   cd $metAnnotateDir
 fi
 
+# The two possible paths to linuxbrew.
+BREW_PATH_ONE=~/.linuxbrew
+BREW_PATH_TWO=/home/linuxbrew/.linuxbrew
+
 echo -e "\nInstalling Linuxbrew\n"
-if [ ! -d "/home/linuxbrew/.linuxbrew" ] && [ ! -d "~/.linuxbrew" ] ; then
+if [ ! -d "$BREW_PATH_ONE" ] && [ ! -d "$BREW_PATH_TWO" ] ; then
     yes | ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
 fi
 
-echo -e "\nAdding Brew to PATH\n"
+echo -e "\nAttempting to add Brew to PATH\n"
 if [ ! `which brew` ] ; then
+    echo -e "\nAdding Brew to PATH\n"
     test -d ~/.linuxbrew && PATH="$HOME/.linuxbrew/bin:$PATH"
     test -d /home/linuxbrew/.linuxbrew && PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
     test -r ~/.bash_profile && echo 'export PATH="$(brew --prefix)/bin:$PATH"' >>~/.bash_profile
