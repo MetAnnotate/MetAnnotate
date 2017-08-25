b5d30e45d243190da122f480a02d810ef90af2ce
Lee Bergstrand <leemacboy@gmail.com>
2017-08-25 13:20:45 -0400

    Move brew installs into their own shell script so they can be separately cached.

---
+++ install_brewed_dependancies.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# The two possible paths to linuxbrew.
+BREW_PATH_ONE=~/.linuxbrew
+BREW_PATH_TWO=/home/linuxbrew/.linuxbrew
+
+echo -e "\nInstalling Linuxbrew\n"
+if [ ! -d "$BREW_PATH_ONE" ] && [ ! -d "$BREW_PATH_TWO" ] ; then
+    yes | ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
+else
+  echo "\nLinux brew is already installed.\n"
+fi
+
+echo -e "\nAttempting to add Brew to PATH\n"
+if [ ! `which brew` ] ; then
+    echo -e "\nAdding Brew to PATH\n"
+    test -d ~/.linuxbrew && PATH="$HOME/.linuxbrew/bin:$PATH"
+    test -d /home/linuxbrew/.linuxbrew && PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
+    test -r ~/.bash_profile && echo 'export PATH="$(brew --prefix)/bin:$PATH"' >>~/.bash_profile
+    echo 'export PATH="$(brew --prefix)/bin:$PATH"' >>~/.profile
+else
+  echo "\nLinux brew is already in the PATH.\n"
+fi
+
+brew tap homebrew/science
+brew install emboss --without-x
