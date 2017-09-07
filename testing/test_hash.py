d8e325b06e0b77903debe238946f9b03dadffbc5
KyleBehiels <kylejamesbehiels@gmail.com>
2017-09-07 11:07:37 -0700

    Fixed docstring

--- testing/test_hash.py
+++ testing/test_hash.py
@@ -4,7 +4,6 @@
 Created by: Metannotate Team (2017)
 
 Description: A simple unittest for testing the hash.py module
-
 """
 
 # Imports:
@@ -21,8 +20,6 @@ class HashTesting(unittest.TestCase):
     def test_hash(self):
         """
         Description: Method that tests the methods 'hexhash()' and 'md5hash()' from module hash
-
-        :param self: Reference to the HashTesting class
         """
         self.assertEqual(hash.hexhash("This is a test string for hexhash"), "46e7d02b9a0d5339")
         self.assertEqual(hash.md5hash("test_constants/test_text_file.txt"), "f5c85408e67ef9a90f3e416863ba84de")
