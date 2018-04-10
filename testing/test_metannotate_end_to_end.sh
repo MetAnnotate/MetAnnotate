32007a424e4fe1363cbcf23309aa042620c96c48
Jackson Tsuji <jackson.tsuji@uwaterloo.ca>
2018-04-10 16:59:24 -0400

    Added code to clear the cache after the test run.

--- testing/test_metannotate_end_to_end.sh
+++ testing/test_metannotate_end_to_end.sh
@@ -20,6 +20,10 @@ fi
 echo "Running Metannotate..."
 python2.7 run_metannotate.py --orf_files=data/MetagenomeTest.fa --hmm_files=data/hmms/RPOB.HMM --reference_database=data/ReferenceTest.fa --output_dir=test_output --tmp_dir=test_tmp --run_mode=both
 
+# Clear the cache to prevent problems when running MetAnnotate with the standard database
+# TODO - update the cache code so that cache UUID's are database-dependent, making this deletion unnecessary.
+rm cache/*
+
 echo "Verifying outputs..."
 
 EXIT_CODE=0
